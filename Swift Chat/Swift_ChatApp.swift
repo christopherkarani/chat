148a9d3c4c2fbce120039931ca69aecc36e80486
christopherkarani <chrisbkarani@gmail.com>
2025-05-20 18:56:43 +0300

    Initial Commit

---
+++ Swift Chat/Swift_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ChatApp.swift
+//  Swift Chat
+//
+//  Created by Chris Karani on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Swift_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift ChatTests/Swift_ChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_ChatTests.swift
+//  Swift ChatTests
+//
+//  Created by Chris Karani on 20/05/2025.
+//
+
+import Testing
+@testable import Swift_Chat
+
+struct Swift_ChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
